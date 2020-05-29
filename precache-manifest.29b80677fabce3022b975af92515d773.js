d7f67d4a127f8b55240e4528deb61797fa9b0411
GermanMunguia <munguiagerman48@gmail.com>
2020-05-28 20:31:38 -0400

    Updates

---
+++ precache-manifest.29b80677fabce3022b975af92515d773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c922aa431f56df0e16a693569ad450d7",
+    "url": "/Click/index.html"
+  },
+  {
+    "revision": "0fa67a676719fb352a84",
+    "url": "/Click/static/css/main.40bc8dd4.chunk.css"
+  },
+  {
+    "revision": "af0fbbb60bf834a6ff1e",
+    "url": "/Click/static/js/2.24061b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Click/static/js/2.24061b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa67a676719fb352a84",
+    "url": "/Click/static/js/main.3b1cc776.chunk.js"
+  },
+  {
+    "revision": "279a6163c3b5603a937c",
+    "url": "/Click/static/js/runtime-main.e622eeeb.js"
+  }
+]);
